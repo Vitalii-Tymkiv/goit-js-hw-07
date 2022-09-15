5d2b8dc246abc33a166f9b497627bda3b053a9ee
Vitalii Tymkiv <vitaliy.tymkiv@gmail.com>
2022-09-15 10:44:50 +0300

    HW 7 Before Mentors Checking

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryRef.addEventListener("click", clickOnGalleryImage);
+
+function createGalleryItem(event) {
+  return event
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function clickOnGalleryImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}' width="800" height="600">
+ `);
+
+  instance.show();
+
+  galleryRef.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
